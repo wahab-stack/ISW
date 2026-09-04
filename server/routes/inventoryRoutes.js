a6facac7b2c6039f25be239155a7333ef8e92be7
Abdul Wahab <wahab.suit997@gmail.com>
2026-09-04 14:30:56 +0500

    Break

--- server/routes/inventoryRoutes.js
+++ server/routes/inventoryRoutes.js
@@ -3,6 +3,9 @@ const express = require("express");
 const {
   createInventory,
   getInventory,
+  getInventoryById,
+  updateInventory,
+  deleteInventory,
 } = require("../controllers/inventoryController");
 
 const router = express.Router();
@@ -12,5 +15,14 @@ router.post("/", createInventory);
 
 // Get all inventory
 router.get("/", getInventory);
+
+// Get inventory by ID
+router.get("/:id", getInventoryById);
+
+// Update inventory
+router.put("/:id", updateInventory);
+
+// Delete inventory
+router.delete("/:id", deleteInventory);
 
 module.exports = router;
